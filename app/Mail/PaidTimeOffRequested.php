a9e8631d95049e4f6815666390beda8502b89bec
Nick Baker <nick@webtechnick.com>
2017-02-26 00:16:25 -0700

    Fixing bug

--- app/Mail/PaidTimeOffRequested.php
+++ app/Mail/PaidTimeOffRequested.php
@@ -31,6 +31,6 @@ class PaidTimeOffRequested extends Mailable
      */
     public function build()
     {
-        return $this->subject('Paid Time Off Request from ' . $pto->employee->name)->view('emails.pto.requested');
+        return $this->subject('Paid Time Off Request from ' . $this->pto->employee->name)->view('emails.pto.requested');
     }
 }
