d64cb1079ebb120bf30f2169679101f013479693
= <=>
2022-10-09 19:38:38 +0000

    Showing Google User

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -55,7 +55,7 @@
                 <!-- Authentication Links -->
                 @if (Auth::guest())
                     @if (isset($user))
-                        <li> <a href="/">{{ $user->name }} |</a> </li>
+                        <li><a href="/" title="Google User">{{ $user->name }}</a></li>
                     @endif
                     <li><a href="{{ url('/login') }}">Login</a></li>
                     <li><a href="{{ url('/register') }}">Register</a></li>
