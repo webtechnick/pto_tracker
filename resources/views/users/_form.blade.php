a8a798133eb7f35ea74be40d36ecd1661615a974
= <=>
2022-10-11 21:51:32 +0000

    No longer forcing user to be default

--- resources/views/users/_form.blade.php
+++ resources/views/users/_form.blade.php
@@ -7,7 +7,7 @@
 
         <div class="form-group">
             <label for="role">Role: </label>
-            {{ Form::select('role', App\User::$roles, 'user', ['class' => 'form-control', 'placeholder' => 'Select One..', 'required']) }}
+            {{ Form::select('role', App\User::$roles, null, ['class' => 'form-control', 'placeholder' => 'Select One..', 'required']) }}
             <span class="help-text">Admin: <small>Manage all PTO, Users, Employees, Holidays, and Teams</small></span><br>
             <span class="help-text">User: <small>No permissions (Default)</small></span>
         </div>
