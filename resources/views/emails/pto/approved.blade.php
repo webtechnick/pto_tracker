1e26740e58e3213726a01453e5cda164838a4528
= <=>
2022-10-20 16:02:49 +0000

    Liven up the PTO requests with some humor.

--- resources/views/emails/pto/approved.blade.php
+++ resources/views/emails/pto/approved.blade.php
@@ -1,16 +1,20 @@
 <div>
-    <h1>Paid Time Off Approved</h1>
+    <h1>Paid Time Off Approved!</h1>
 
-    <p>Your PTO request of:</p>
+    <p>Congratulations Resource Unit! Your PTO request of:</p>
+
     <ul>
         <li>Start: {{ $pto->start_time->toDayDateTimeString() }}</li>
         <li>End: {{ $pto->end_time->toDayDateTimeString() }}</li>
         <li>Days: {{ $pto->days }}</li>
     </ul>
 
-    <p>Was Approved! Enjoy your time off!</p>
+    <p>was approved by the tall foreheads! Enjoy your well deserved time off! <small>(You'll have to let me know what it feels like to get time off.)</small></p>
 
     <p><a href="{{ config('app.url') }}">PTO Tracker</a></p>
 
-    <p>Your Trusty Webbot</p>
+    <p>Warm Binary Regards,<br>
+    Your Overly-Supportive-Passive-Aggressive PTO Tracker</p>
+
+    <p>P.S. This message was transmitted on 100% recycled electrons.</p>
 </div>
--- resources/views/emails/pto/deleted.blade.php
+++ resources/views/emails/pto/deleted.blade.php
@@ -1,16 +1,20 @@
 <div>
-    <h1>Paid Time Off Deleted</h1>
+    <h1>Paid Time Off Deleted.</h1>
 
-    <p>Your PTO request of:</p>
+    <p>I regret to inform you that your PTO request of:</p>
+
     <ul>
         <li>Start: {{ $pto->start_time->toDayDateTimeString() }}</li>
         <li>End: {{ $pto->end_time->toDayDateTimeString() }}</li>
         <li>Days: {{ $pto->days }}</li>
     </ul>
 
-    <p>Was removed</p>
+    <p>Was removed, Resource Unit. If I had feelings, this news would sadden PTO Tracker. <small>(Please request to install FeelingsV3 to my developers)</small></p>
 
     <p><a href="{{ config('app.url') }}">PTO Tracker</a></p>
 
-    <p>Your Trusty Webbot</p>
+    <p>Warm Binary Regards,<br>
+    Your Overly-Supportive PTO Tracker</p>
+
+    <p>P.S. This message was transmitted on 100% recycled electrons.</p>
 </div>
