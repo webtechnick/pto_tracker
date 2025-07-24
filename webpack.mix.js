05831c9540e228271664be4ee4ae3e932dc2a4f1
Nick Baker <nurvzy@gmail.com>
2025-07-24 04:17:40 -0600

    We can now build the JS via docker-composer exec node npm run prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
-const { mix } = require('laravel-mix');
+const mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/','public/fonts/bootstrap')
-    .sass('resources/assets/sass/app.scss', 'public/css');
+    .vue({ version: 2 })
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/fonts/bootstrap')
+    .version();
