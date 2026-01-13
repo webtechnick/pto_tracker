f16a02c7d0232e7cb879213e07b049d282ac9517
Nick Baker <nurvzy@gmail.com>
2026-01-13 00:08:42 -0700

    Updating our seeders.

--- database/seeds/HolidaySeeder.php
+++ database/seeds/HolidaySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Holiday;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class HolidaySeeder extends Seeder
@@ -12,21 +13,94 @@ class HolidaySeeder extends Seeder
      */
     public function run()
     {
+        $year = date('Y');
+
         $holidays = [
-            ['title' => 'New Year', 'date' => '2017-01-02'],
-            ['title' => 'Good Friday', 'date' => '2017-04-14'],
-            ['title' => 'Memorial Day', 'date' => '2017-05-29'],
-            ['title' => 'Indepedence Day', 'date' => '2017-07-04'],
-            ['title' => 'Indepedence Day', 'date' => '2017-07-03'],
-            ['title' => 'Labor Day', 'date' => '2017-09-04'],
-            ['title' => 'Thanksgiving', 'date' => '2017-11-23'],
-            ['title' => 'Thanksgiving', 'date' => '2017-11-24'],
-            ['title' => 'Christmas', 'date' => '2017-12-25'],
-            ['title' => 'Christmas', 'date' => '2017-12-26'],
+            // Fixed holidays
+            ['title' => 'New Year\'s Day', 'date' => $this->observedDate("{$year}-01-01")],
+            ['title' => 'Independence Day', 'date' => $this->observedDate("{$year}-07-04")],
+            ['title' => 'Christmas Eve', 'date' => "{$year}-12-24"],
+            ['title' => 'Christmas Day', 'date' => $this->observedDate("{$year}-12-25")],
+            ['title' => 'Day After Christmas', 'date' => "{$year}-12-26"],
+
+            // Floating holidays
+            ['title' => 'Good Friday', 'date' => $this->getGoodFriday($year)],
+            ['title' => 'Memorial Day', 'date' => $this->getMemorialDay($year)],
+            ['title' => 'Labor Day', 'date' => $this->getLaborDay($year)],
+            ['title' => 'Thanksgiving', 'date' => $this->getThanksgiving($year)],
+            ['title' => 'Day After Thanksgiving', 'date' => $this->getDayAfterThanksgiving($year)],
         ];
 
         foreach ($holidays as $holiday) {
             Holiday::create($holiday);
         }
+
+        // Add summer half-day Fridays (June through August)
+        Holiday::bulkHalfFromRequest([
+            'start' => "{$year}-06-01",
+            'end' => "{$year}-08-31",
+        ]);
+    }
+
+    /**
+     * Get the observed date for a holiday (handles weekends)
+     */
+    private function observedDate($date)
+    {
+        $carbon = Carbon::parse($date);
+        
+        // If Saturday, observe on Friday
+        if ($carbon->isSaturday()) {
+            return $carbon->subDay()->toDateString();
+        }
+        
+        // If Sunday, observe on Monday
+        if ($carbon->isSunday()) {
+            return $carbon->addDay()->toDateString();
+        }
+
+        return $carbon->toDateString();
+    }
+
+    /**
+     * Get Good Friday (Friday before Easter)
+     */
+    private function getGoodFriday($year)
+    {
+        $easter = Carbon::createFromTimestamp(easter_date($year));
+        return $easter->subDays(2)->toDateString();
+    }
+
+    /**
+     * Get Memorial Day (last Monday of May)
+     */
+    private function getMemorialDay($year)
+    {
+        return Carbon::parse("last monday of may {$year}")->toDateString();
+    }
+
+    /**
+     * Get Labor Day (first Monday of September)
+     */
+    private function getLaborDay($year)
+    {
+        return Carbon::parse("first monday of september {$year}")->toDateString();
+    }
+
+    /**
+     * Get Thanksgiving (fourth Thursday of November)
+     */
+    private function getThanksgiving($year)
+    {
+        return Carbon::parse("fourth thursday of november {$year}")->toDateString();
+    }
+
+    /**
+     * Get Day After Thanksgiving (Friday)
+     */
+    private function getDayAfterThanksgiving($year)
+    {
+        $thanksgiving = Carbon::parse("fourth thursday of november {$year}");
+        return $thanksgiving->addDay()->toDateString();
     }
 }
--- database/seeds/PaidTimeOffSeeder.php
+++ database/seeds/PaidTimeOffSeeder.php
@@ -13,90 +13,98 @@ class PaidTimeOffSeeder extends Seeder
      */
     public function run()
     {
+        $year = date('Y');
+
+        // PTO data uses month-day format, year is added dynamically
         $paidtimeoffs = [
-            'Aaron Hall' => [
-                ['start_time' => '2017-01-25', 'end_time' => '2017-01-25'],
-                ['start_time' => '2017-02-24', 'end_time' => '2017-02-24'],
+            'Desmond Rush' => [
+                ['start' => '02-14', 'end' => '02-14'],
+                ['start' => '06-20', 'end' => '06-24'],
+                ['start' => '11-27', 'end' => '11-29'],
             ],
-            'Ben Stout' => [
-                ['start_time' => '2017-02-20', 'end_time' => '2017-02-20'],
-                ['start_time' => '2017-03-20', 'end_time' => '2017-03-24'],
-                ['start_time' => '2017-04-17', 'end_time' => '2017-04-17'],
-                ['start_time' => '2017-06-09', 'end_time' => '2017-06-09'],
-                ['start_time' => '2017-07-10', 'end_time' => '2017-07-14'],
-                ['start_time' => '2017-12-20', 'end_time' => '2017-12-29'],
-            ],
-            'Craig Knowles' => [
+            'Drew Glaser' => [
+                ['start' => '03-15', 'end' => '03-15'],
+                ['start' => '07-03', 'end' => '07-07'],
+                ['start' => '12-23', 'end' => '12-27'],
             ],
             'Dillon Formo' => [
-                ['start_time' => '2017-04-17', 'end_time' => '2017-04-17'],
-                ['start_time' => '2017-06-02', 'end_time' => '2017-06-02'],
-                ['start_time' => '2017-07-05', 'end_time' => '2017-07-05'],
-                ['start_time' => '2017-11-27', 'end_time' => '2017-11-27'],
-                ['start_time' => '2017-12-22', 'end_time' => '2017-12-27'],
+                ['start' => '04-17', 'end' => '04-17'],
+                ['start' => '06-02', 'end' => '06-02'],
+                ['start' => '07-05', 'end' => '07-05'],
+                ['start' => '11-27', 'end' => '11-27'],
+                ['start' => '12-22', 'end' => '12-27'],
             ],
-            'Jeff Kolesnikowicz' => [
+            'Elanor Riley' => [
+                ['start' => '01-15', 'end' => '01-15'],
+                ['start' => '05-24', 'end' => '05-28'],
+                ['start' => '09-04', 'end' => '09-04'],
             ],
-            'Laura Kimpel-Matthews' => [
-                ['start_time' => '2017-05-24', 'end_time' => '2017-05-31'],
-                ['start_time' => '2017-08-28', 'end_time' => '2017-08-31'],
-                ['start_time' => '2017-11-20', 'end_time' => '2017-11-22'],
-                ['start_time' => '2017-12-27', 'end_time' => '2017-12-29'],
+            'Eric Pridham' => [
+                ['start' => '02-20', 'end' => '02-21'],
+                ['start' => '08-14', 'end' => '08-18'],
+                ['start' => '12-26', 'end' => '12-29'],
+            ],
+            'Joey Agpawa' => [
+                ['start' => '03-10', 'end' => '03-10'],
+                ['start' => '06-19', 'end' => '06-23'],
+                ['start' => '11-22', 'end' => '11-24'],
             ],
             'Lyle Smart' => [
-                ['start_time' => '2017-05-30', 'end_time' => '2017-05-31'],
-                ['start_time' => '2017-06-01', 'end_time' => '2017-06-09'],
-                ['start_time' => '2017-12-20', 'end_time' => '2017-12-29'],
+                ['start' => '05-30', 'end' => '05-31'],
+                ['start' => '06-01', 'end' => '06-09'],
+                ['start' => '12-20', 'end' => '12-29'],
             ],
             'Mark Vince' => [
-                ['start_time' => '2017-01-25', 'end_time' => '2017-01-25'],
-                ['start_time' => '2017-03-23', 'end_time' => '2017-03-24'],
-                ['start_time' => '2017-04-03', 'end_time' => '2017-04-07'],
-                ['start_time' => '2017-10-09', 'end_time' => '2017-10-13'],
-                ['start_time' => '2017-11-22', 'end_time' => '2017-11-22'],
-                ['start_time' => '2017-12-21', 'end_time' => '2017-12-29'],
+                ['start' => '01-25', 'end' => '01-25'],
+                ['start' => '03-23', 'end' => '03-24'],
+                ['start' => '04-03', 'end' => '04-07'],
+                ['start' => '10-09', 'end' => '10-13'],
+                ['start' => '11-22', 'end' => '11-22'],
+                ['start' => '12-21', 'end' => '12-29'],
             ],
             'Nick Baker' => [
-                ['start_time' => '2017-02-20', 'end_time' => '2017-02-21'],
-                ['start_time' => '2017-03-01', 'end_time' => '2017-03-01'],
-                ['start_time' => '2017-05-25', 'end_time' => '2017-05-31'],
-                ['start_time' => '2017-08-11', 'end_time' => '2017-08-14'],
-                ['start_time' => '2017-12-18', 'end_time' => '2017-12-29'],
+                ['start' => '02-20', 'end' => '02-21'],
+                ['start' => '03-01', 'end' => '03-01'],
+                ['start' => '05-25', 'end' => '05-31'],
+                ['start' => '08-11', 'end' => '08-14'],
+                ['start' => '12-18', 'end' => '12-29'],
             ],
             'Rachelle Zani' => [
-                ['start_time' => '2017-02-09', 'end_time' => '2017-02-09'],
-                ['start_time' => '2017-07-05', 'end_time' => '2017-07-14'],
-                ['start_time' => '2017-08-21', 'end_time' => '2017-08-21'],
-                ['start_time' => '2017-09-21', 'end_time' => '2017-09-21'],
-                ['start_time' => '2017-11-22', 'end_time' => '2017-11-22'],
-                ['start_time' => '2017-12-27', 'end_time' => '2017-12-29'],
+                ['start' => '02-09', 'end' => '02-09'],
+                ['start' => '07-05', 'end' => '07-14'],
+                ['start' => '08-21', 'end' => '08-21'],
+                ['start' => '09-21', 'end' => '09-21'],
+                ['start' => '11-22', 'end' => '11-22'],
+                ['start' => '12-27', 'end' => '12-29'],
             ],
-            'Travis Reddell' => [
-                ['start_time' => '2017-02-03', 'end_time' => '2017-02-03'],
-                ['start_time' => '2017-02-24', 'end_time' => '2017-02-24'],
-                ['start_time' => '2017-03-15', 'end_time' => '2017-03-15'],
-                ['start_time' => '2017-04-07', 'end_time' => '2017-04-07'],
-                ['start_time' => '2017-04-28', 'end_time' => '2017-04-28'],
-                ['start_time' => '2017-05-19', 'end_time' => '2017-05-19'],
-                ['start_time' => '2017-06-09', 'end_time' => '2017-06-09'],
-                ['start_time' => '2017-09-11', 'end_time' => '2017-09-15'],
-                ['start_time' => '2017-10-20', 'end_time' => '2017-10-20'],
-                ['start_time' => '2017-12-21', 'end_time' => '2017-12-29'],
+            'Valerie England' => [
+                ['start' => '02-03', 'end' => '02-03'],
+                ['start' => '04-28', 'end' => '04-28'],
+                ['start' => '09-11', 'end' => '09-15'],
+                ['start' => '12-21', 'end' => '12-29'],
             ],
             'Tyler Shaw' => [
-                ['start_time' => '2017-02-09', 'end_time' => '2017-02-14'],
-                ['start_time' => '2017-07-05', 'end_time' => '2017-07-07'],
-                ['start_time' => '2017-11-20', 'end_time' => '2017-11-22'],
-                ['start_time' => '2017-12-27', 'end_time' => '2017-12-29'],
+                ['start' => '02-09', 'end' => '02-14'],
+                ['start' => '07-05', 'end' => '07-07'],
+                ['start' => '11-20', 'end' => '11-22'],
+                ['start' => '12-27', 'end' => '12-29'],
             ],
         ];
 
-        foreach ($paidtimeoffs as $employee_name => $ptos) {
-            $id = Employee::where('name', $employee_name)->getField('id');
+        foreach ($paidtimeoffs as $employeeName => $ptos) {
+            $employee = Employee::where('name', $employeeName)->first();
+            
+            if (!$employee) {
+                continue;
+            }
+
             foreach ($ptos as $pto) {
-                $pto['employee_id'] = $id;
-                $pto['is_approved'] = true;
-                PaidTimeOff::create($pto);
+                PaidTimeOff::create([
+                    'employee_id' => $employee->id,
+                    'start_time' => "{$year}-{$pto['start']}",
+                    'end_time' => "{$year}-{$pto['end']}",
+                    'is_approved' => true,
+                ]);
             }
         }
     }
